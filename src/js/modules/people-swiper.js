c59839d1b34f1efff82830107b24dcce0b21a466
Łukasz Goral <lukasz.goral.design@gmail.com>
2020-05-22 12:27:31 +0200

    updates

--- src/js/modules/people-swiper.js
+++ src/js/modules/people-swiper.js
@@ -1,45 +1,67 @@
 import Swiper from 'swiper';
-class peopleSwiper{
+class peopleSwiper {
 
-constructor() {
-  this.peopleSlider = null;
+    constructor() {
+        this.peopleSlider = null;
+        this.peopleList = null;
+        this.swiper = null;
 
-}
-init() {
-this.peopleSlider = document.querySelector('[data-people-slider]');
-if(this.peopleSlider) {
-  this.initBindings();
-  this.initSwiper();
-}
-}
+    }
+    init() {
+        this.peopleSlider = document.querySelector('[data-people-slider]');
+        this.peopleList = document.querySelector('[data-people-list]');
+        if (this.peopleSlider && this.peopleList) {
+            this.initBindings();
+            this.initSwiper();
+        }
+    }
 
-initBindings() {
-  this.peopleSlider.querySelector('[data-close]').addEventListener('click', ()=>{
-    alert('clicked')
-  })
-}
+    initBindings() {
+        this.peopleSlider.querySelector('[data-close]').addEventListener('click', () => {
+            this.closeModal();
+        })
+        console.log(this.peopleList.querySelectorAll('[data-person]'));
+        this.peopleList.querySelectorAll('[data-person]').forEach((element, index) => {
+            element.addEventListener('click', () => {
+                this.openModal(index + 1);
 
-initSwiper() {
-  const mySwiper = new Swiper ('.people-slider__container', {
-    // Optional parameters
-  
-    loop: true,
+            })
+        });
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+    }
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+    openModal(slide) {
+        this.peopleSlider.classList.add('open')
+        this.peopleSlider.tabIndex = "0"
+        this.swiper.slideTo(slide, 0);
+    }
+    closeModal() {
+        this.peopleSlider.classList.remove('open')
+        this.peopleSlider.tabIndex = "-1"
 
-    // And if we need scrollbar
+    }
 
-  })
-}
+    initSwiper() {
+        this.swiper = new Swiper('.people-slider__container', {
+            // Optional parameters
+
+            loop: true,
+
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+            },
+
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+
+            // And if we need scrollbar
+
+        })
+    }
 
 }
 
